76afe52e19e5b23e34d2d9ddbb5a07447c833b97
gkofgibff <128242588+gkofgibff@users.noreply.github.com>
2023-03-19 02:28:46 +0800

    Update build_url.js

--- assets/js/build_url.js
+++ assets/js/build_url.js
@@ -1,10 +1,40 @@
+function jiami(str, num) {
+    var newStr = "";
+    for (let i = 0; i < str.length; i++) {
+      if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) {
+        newStr += String.fromCharCode((str.charCodeAt(i) - 65 + num + 26) % 26 + 65)
+      }
+      else if (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122) {
+        newStr += String.fromCharCode((str.charCodeAt(i) - 97 + num + 26) % 26 + 97)
+      }
+      //特殊符号不做处理
+      else newStr += String.fromCharCode(str.charCodeAt(i));
+    }
+    // console.log(newStr);
+    return newStr;
+  }
+
+function jiemi(str, num) {
+    var newStr = "";
+    for (let i = 0; i < str.length; i++) {
+        // str.chatCodeAt(i);
+        if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) {
+        newStr += String.fromCharCode((str.charCodeAt(i) - 65 - num + 26) % 26 + 65)
+        }
+        else if (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122) {
+        newStr += String.fromCharCode((str.charCodeAt(i) - 97 - num + 26) % 26 + 97)
+        }
+        //特殊符号不做处理
+        else newStr += String.fromCharCode(str.charCodeAt(i));
+    }
+    // console.log(newStr);
+    return newStr;
+}
+
 function build_url(){
-    var url = encodeURI(decodeURI(document.querySelector('#url').value));
-    if (url === "" || url.indexOf("http") === -1){
-        document.getElementById("b_url").innerHTML=`输入的不是链接或者未加http请求头！`;
-    }
-    else {
-        url = document.location.href + "api/?username=" + url;
-        document.getElementById("b_url").innerHTML=`<a href=${url} target='_blank'>${url}</a>`;
-    }
+    var url = encodeURI(document.querySelector('#url').value);
+    url = jiami(url,5)
+    url = "https://qiantigers.github.io/qq_fanghong/api/?url=" + window.btoa(url);
+    console.log(url)
+    document.getElementById("b_url").innerHTML=`<a href=${url} target='_blank'>${url}</a>`;
 }
