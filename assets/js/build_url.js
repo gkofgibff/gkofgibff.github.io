fed40ccd19ed93d57b061edefacfdc13b216b5ed
gkofgibff <128242588+gkofgibff@users.noreply.github.com>
2023-03-19 02:41:03 +0800

    Update build_url.js

--- assets/js/build_url.js
+++ assets/js/build_url.js
@@ -33,7 +33,7 @@ function jiemi(str, num) {
 
 function build_url(){
     var url = encodeURI(document.querySelector('#url').value);
-    url = "https://qiantigers.github.io/qq_fanghong/api/?url=" + this.jiami(url,5);
+    url = document.location.href + 'api/?' + this.jiami(url,5);
     console.log(url)
     document.getElementById("b_url").innerHTML=`<a href=${url} target='_blank'>${url}</a>`;
 }
